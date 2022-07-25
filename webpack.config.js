1116969bf23c31176a3d1dfef846fd20e7f4a6d3
jindory <laonjay@gmail.com>
2022-07-25 15:47:22 +0900

    실험👀

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  target: ['web', 'es5'],
   entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
   output: {
     path: path.resolve(__dirname, 'docs'), // './dist'의 절대 경로를 리턴합니다.
     filename: 'app.bundle.js',
